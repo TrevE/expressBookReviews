136157a0a555d0e5878e1f5fae07815cda2b34ac
Trevor Eckhart <254928+TrevE@users.noreply.github.com>
2026-07-12 12:26:14 -0400

    Add functionality to get book reviews by ISBN
    Implement book review retrieval based on ISBN.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,8 +67,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    var isbn = req.params.isbn;
+    var book = books[isbn];
+    
+    if (book) {
+        return res.status(200).json(book.reviews);
+    } else {
+        return res.status(404).json({ message: "Book was not found" });
+    }
 });
 
 module.exports.general = public_users;
